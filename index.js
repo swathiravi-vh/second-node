1b36ddef0e5dd3daeaa969881e71c10ec537dcb6
swathiravi-vh <swathiit01@gmail.com>
2022-11-21 19:57:04 +0530

    initial

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express")
+const cors = require("cors");
+const app = express()
+
+//middlware(use)
+app.use(cors({
+    origin : "http://localhost:3001"
+}))
+
+app.use(express.json());
+
+let products = [];
+
+//create
+app.post("/product",(req,res) => {
+  req.body.id = products.length + 1;
+    products.push(req.body)
+  res.json({message:"Product Added"})
+})
+
+//read
+app.get("/products",(req,res) =>{
+    res.json(products)
+})
+
+//URL parameter
+app.put("/product/:productId" , (req,res) => {
+    const productId = req.params.productId;
+    
+    const productIndex = products.findIndex((prod) => prod.id == productId)
+    
+    const keys = Object.keys(req.body)
+    keys.forEach((key) => {
+      products[productIndex][key] = req.body[key]
+    })
+    //products[productIndex].price = req.body.price
+    res.json({message:"done"})
+})
+
+//Read product
+app.get("/product/:productId" ,(req,res) => {
+  const productId = req.params.productId;
+  const productIndex = products.findIndex((prod) => prod.id == productId)
+  res.json(products[productIndex])
+})
+
+app.delete("/product/:productId" , (req,res) => {
+ const productId = req.params.productId;
+ const productIndex = products.findIndex((prod) => prod.id == productId);
+ products.splice(productIndex ,1);
+ res.json({message : "Deleted"});
+})
+
+
+app.listen(3000) //anything bw 3000 to 8000
